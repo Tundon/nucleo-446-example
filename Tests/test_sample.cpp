d70521970221f39cb60525fcb9c1cf3009a54f42
Yun Jia <yunjia@me.com>
2023-05-31 10:25:33 -0700

    chore: A working test setup.

--- Tests/test_sample.cpp
+++ Tests/test_sample.cpp
@@ -6,5 +6,11 @@
 TEST(ExampleTest, TestAddition) {
   int a = 2;
   int b = 3;
-  EXPECT_EQ(a + b, 6);
+  EXPECT_EQ(a + b, 5);
+}
+
+TEST(ExampleTest, TestAddition2) {
+  int a = 2;
+  int b = 3;
+  EXPECT_EQ(a + b, 4);
 }
